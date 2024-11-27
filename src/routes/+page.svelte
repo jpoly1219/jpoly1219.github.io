ba1d1dabcb7f51c8622810b8d30817c1c2d1767f
jpoly1219 <jpoly@umich.edu>
2024-11-26 19:00:44 -0500

    create container divs

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,28 @@
-<h1>Jacob's Personal Site</h1>
-<p>Hi! This page is currently under construction.</p>
-<p>Come back soon!</p>
+<div class="container">
+  <div class="left">
+    <p>image goes here</p>
+  </div>
+  <div class="right">
+    <div class="about">
+      <h1>Jacob's Personal Site</h1>
+      <p>Hi! This page is currently under construction.</p>
+      <p>Come back soon!</p>
+    </div>
+    <div class="projects"></div>
+  </div>
+</div>
+
+<style>
+  .container {
+    display: flex;
+    flex-direction: row;
+  }
+
+  .left {
+    width: 50%;
+  }
+
+  .right {
+    width: 50%;
+  }
+</style>
