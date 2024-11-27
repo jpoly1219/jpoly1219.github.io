3cfc721be5f9260ab2d43addea34e3bbeb378d93
Jacob Kim <jpoly1219@gmail.com>
2024-11-26 20:53:49 -0500

    create about page component

---
+++ src/components/About.svelte
@@ -0,0 +1,12 @@
+<div class="about">
+  <h1>Jacob's Personal Site</h1>
+  <p>Hi! This page is currently under construction.</p>
+  <p>Come back soon!</p>
+</div>
+
+<style>
+  .about {
+    width: 100%;
+    height: 100%;
+  }
+</style>
