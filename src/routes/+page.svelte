834fa971e2569bc03f7a6fd3415454858ee0206b
Jacob Kim <jpoly1219@gmail.com>
2024-11-26 20:53:24 -0500

    import scrollable div

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,28 +1,41 @@
+<script>
+  import ScrollableDiv from "../components/ScrollableDiv.svelte";
+
+  const sections = ["about", "projects"];
+</script>
+
 <div class="container">
   <div class="left">
     <p>image goes here</p>
   </div>
   <div class="right">
-    <div class="about">
-      <h1>Jacob's Personal Site</h1>
-      <p>Hi! This page is currently under construction.</p>
-      <p>Come back soon!</p>
-    </div>
-    <div class="projects"></div>
+    <ScrollableDiv sectionTitles={sections} />
   </div>
 </div>
 
 <style>
+  :global(body) {
+    margin: 0;
+    padding: 0;
+    overflow: hidden;
+    background-color: #fdf6e3;
+  }
+
   .container {
     display: flex;
     flex-direction: row;
+    width: 100vw;
+    height: 100vh;
   }
 
   .left {
     width: 50%;
+    height: 100%;
   }
 
   .right {
     width: 50%;
+    height: 100%;
+    min-height: 100%;
   }
 </style>
