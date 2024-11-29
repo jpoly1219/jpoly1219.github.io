18edbf9fa6ecaab1b8a3926ab6a719f2361dcaeb
Jacob Kim <jpoly1219@gmail.com>
2024-11-29 02:30:06 -0500

    create card component

---
+++ src/components/Card.svelte
@@ -0,0 +1,41 @@
+<script lang="ts">
+  const { title, employer, desc }: { title: string; employer: string; desc: string[] } = $props();
+</script>
+
+<div class="card">
+  <h2 class="pacifico-regular">
+    {title}
+  </h2>
+  <p class="quicksand">
+    {#if employer.slice(0, 8) === "https://"}
+      <a href={employer}>{employer}</a>
+    {:else}
+      {employer}
+    {/if}
+  </p>
+  {#each desc as d}
+    <p class="quicksand">{d}</p>
+  {/each}
+</div>
+
+<style>
+  .pacifico-regular {
+    font-family: "Pacifico", cursive;
+    font-weight: 400;
+    font-style: normal;
+  }
+
+  .lora {
+    font-family: "Lora", serif;
+    font-optical-sizing: auto;
+    font-weight: 400;
+    font-style: normal;
+  }
+
+  .quicksand {
+    font-family: "Quicksand", sans-serif;
+    font-optical-sizing: auto;
+    font-weight: 500;
+    font-style: normal;
+  }
+</style>
